cfca89224aed90f96c239b5402275aabb8c63733
VainF <2218880241@qq.com>
2023-05-19 02:28:46 +0800

    update

--- scripts/sample_ddpm_cifar10_pretrained_distributed.sh
+++ scripts/sample_ddpm_cifar10_pretrained_distributed.sh
@@ -1,4 +1,4 @@
 python -m torch.distributed.launch --nproc_per_node=8 --master_port 22222 --use_env ddpm_sample.py \
---output_dir run/cifar10_sample \
+--output_dir run/sample/ddpm_cifar10_pretrained \
 --model_path pretrained/ddpm_ema_cifar10 \
 --batch_size 128 \
